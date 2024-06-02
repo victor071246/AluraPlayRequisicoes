550e5b970a9c835947c8df81feef05d07ce735d0
Victor Dadério <155397909+victor071246@users.noreply.github.com>
2024-06-01 23:46:05 -0300

    Add files via upload

---
+++ js/buscarVideo.js
@@ -0,0 +1,15 @@
+import { conectaApi } from "./conectaApi.js";
+import { constroiCard } from "./mostrarVideos.js";
+
+async function buscarVideo(evento) {
+    evento.preventDefault();
+
+    const dadosDePesquisa = document.querySelector("[data-pesquisa]").value;
+    const busca = await conectaApi.buscaVideo(dadosDePesquisa);
+
+    console.log(busca)
+}
+
+const botaoDePesquisa = document.querySelector("[data-botao-pesquisa]")
+
+botaoDePesquisa.addEventListener("click", evento => buscarVideo(evento));
